ee11f9ac0b79323ae3c92112a4cbbeec63499667
Vyacheslav Troitskiy <vt@kwel.ru>
2020-05-05 18:30:52 +0300

    Third task

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,6 @@
 #!/usr/bin/env node
 
-console.log('Welcome to the Brain Games!');
+import * as cli from '../src/cli.js';
+
+cli.sayWelcome();
+cli.greetOfUser();
---
+++ src/cli.js
@@ -0,0 +1,15 @@
+import readlineSync from 'readline-sync';
+
+const sayWelcome = () => console.log('Welcome to the Brain Games!');
+
+
+
+const greetOfUser = () => {
+    const name = readlineSync.question('What is your name? ');
+    console.log(`Yo, ${name}!`);
+};
+
+export {
+    sayWelcome,
+    greetOfUser,
+};
